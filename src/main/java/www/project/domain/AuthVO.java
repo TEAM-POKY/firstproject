496a8d36634dba0953086c44407df71894ad13f7
gajami-kim <qansd38@gmail.com>
2024-07-13 20:27:51 +0900

    정온 로그인완

--- src/main/java/www/project/domain/AuthVO.java
+++ src/main/java/www/project/domain/AuthVO.java
@@ -6,7 +6,6 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class AuthVO {
     private String email;
     private String role;
--- src/main/java/www/project/security/CustomUserService.java
+++ src/main/java/www/project/security/CustomUserService.java
@@ -1,23 +1,21 @@
 package www.project.security;
 
-
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import www.project.domain.UserVO;
 import www.project.repository.UserMapper;
 
-@RequiredArgsConstructor
 @Slf4j
 public class CustomUserService implements UserDetailsService {
-    private final UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserVO uvo = userMapper.checkEmail(email);
-        log.info("login uvo>>>>>>>{}",uvo);
         uvo.setAuthList(userMapper.selectAuth(email));
         return (UserDetails) new AuthUser(uvo);
     }
