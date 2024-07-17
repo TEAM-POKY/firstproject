e92a4b6ccb5b9bf03494b8a9466b831e2c5ab771
ehdwo13 <ehdwo13@gmail.com>
2024-07-18 04:51:52 +0900

    complete profile image add + modify + display function

--- src/main/java/www/project/config/WebMvcConfig.java
+++ src/main/java/www/project/config/WebMvcConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    // 정적 리소스로 사용할 파일 시스템 경로
     String uploadPath = "file:///C:\\image\\";
 
     @Override
